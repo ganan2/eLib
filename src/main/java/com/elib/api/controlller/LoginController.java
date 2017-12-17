e941d9dcf7f9746bdc49d9a32be7cbce140cb1df
ganan2 <ganan2@uis.edu>
2017-12-16 22:38:13 -0600

    Created login and signup forms and controllers

---
+++ src/main/java/com/elib/api/controlller/LoginController.java
@@ -0,0 +1,19 @@
+package com.elib.api.controlller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping("/login")
+	public String home() {
+		return "login";
+	}
+	
+	@RequestMapping("/")
+	public String login() {
+		return "redirect:/login";
+	}
+
+}
