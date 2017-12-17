814612e3b0f91aff959fddc7602211be7314a8f1
ganan2 <ganan2@uis.edu>
2017-12-17 03:33:26 -0600

    refactored login and signup

--- src/main/java/com/elib/api/config/SecurityConfig.java
+++ src/main/java/com/elib/api/config/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.elib.api.controlller.SignUpController;
 import com.elib.api.service.serviceImpl.UserSecurityServiceImpl;
 
 @Configuration
