aa7c0e092565e50178d3f205b75d7ae6d3135561
ganan2 <ganan2@uis.edu>
2017-12-30 08:26:57 -0600

    refactored add controller

--- src/main/java/com/elib/api/controller/AddController.java
+++ src/main/java/com/elib/api/controller/AddController.java
@@ -1,15 +1,16 @@
 package com.elib.api.controller;
 
-import com.elib.api.domain.User;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AddController {
-    
+
     @RequestMapping(value = "/add")
-    public @ResponseBody String getContact(@RequestParam("username") String username){
+    public @ResponseBody
+    String getContact(@RequestParam("username") String username) {
 
         return "Added";
     }
