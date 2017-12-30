118df96dd687a996ba7dc1a1b798bd2f7e05c76e
ganan2 <ganan2@uis.edu>
2017-12-29 22:59:33 -0600

    refactored add contact

--- src/main/java/com/elib/api/controller/AddController.java
+++ src/main/java/com/elib/api/controller/AddController.java
@@ -1,18 +1,16 @@
 package com.elib.api.controller;
 
+import com.elib.api.domain.User;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AddController {
-
-    private org.slf4j.Logger LOG = LoggerFactory.getLogger(AddController.class);
-
+    
     @RequestMapping(value = "/add")
     public @ResponseBody String getContact(@RequestParam("username") String username){
 
-        String status = "Added";
-        return status;
+        return "Added";
     }
 }
--- src/main/resources/static/js/contacts/ajaxFunction.js
+++ src/main/resources/static/js/contacts/ajaxFunction.js
@@ -1,6 +1,6 @@
 function pushToContacts(username) {
   var xhttp = new XMLHttpRequest();
-  alert(username);
+  //alert(username);
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("add").innerHTML =
